23a8c0ca9298aae341622794ac0bcdbd04e1bfe0
Benoît Legat <benoit.legat@gmail.com>
2025-03-21 10:37:29 +0100

    Add instantiate step to pluto_gpu1

--- Lectures/pluto_gpu1.sh
+++ Lectures/pluto_gpu1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=pluto_gpu1
 #SBATCH --ntasks=1
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH --mem-per-cpu=10000
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 
 ip=$(hostname -i)
 echo "Run \$ sshuttle -r $CLUSTER_NAME $ip/16"
-srun julia --project -e "import Pluto; Pluto.run(host=\"$ip\", port=1234, launch_browser=false)"
+julia --project -e 'import Pkg; Pkg.instantiate()'
+julia --project -e "import Pluto; Pluto.run(host=\"$ip\", port=1234, launch_browser=false)"
